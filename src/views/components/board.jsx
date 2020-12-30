83632fa5a218190ce7a40f2e01aee03da5ff54b6
Ryo Katsuse <ryokatsu0719@gmail.com>
2020-12-30 11:46:31 +0900

    feat: 盤面をみて勝ち負けを判定するロジックを実装

--- src/views/components/board.jsx
+++ src/views/components/board.jsx
@@ -1,18 +1,26 @@
 import React from 'react'
 import Square from './square'
+import { calculateWinner } from '../../utils/calclateWinner';
 
 class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true
     };
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSquare(i) {
@@ -25,7 +33,13 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div>
--- src/views/components/square.jsx
+++ src/views/components/square.jsx
@@ -1,17 +1,5 @@
 import React from 'react'
 
-// class Square extends React.Component {
-//   render() {
-//     const handler = () => {
-//       this.props.onClick()
-//     }
-//     return (
-//       <button className="square" onClick={handler}>
-//         {this.props.value}
-//       </button>
-//     )
-//   }
-// }
 function Square(props) {
   const handler = () => {
     props.onClick()
