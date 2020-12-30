3edf4e3e4c048d00e1e2b0b75b3f901e10b7dc36
Ryo Katsuse <ryokatsu0719@gmail.com>
2020-12-30 11:29:23 +0900

    refacotr:square.jsxを関数コンポーネントに置き換えた

--- src/views/components/square.jsx
+++ src/views/components/square.jsx
@@ -1,16 +1,26 @@
 import React from 'react'
 
-class Square extends React.Component {
-  render() {
-    const handler = () => {
-      this.props.onClick()
-    }
-    return (
-      <button className="square" onClick={handler}>
-        {this.props.value}
-      </button>
-    )
+// class Square extends React.Component {
+//   render() {
+//     const handler = () => {
+//       this.props.onClick()
+//     }
+//     return (
+//       <button className="square" onClick={handler}>
+//         {this.props.value}
+//       </button>
+//     )
+//   }
+// }
+function Square(props) {
+  const handler = () => {
+    props.onClick()
   }
+  return (
+    <button className="square" onClick={handler}>
+      {props.value}
+    </button>
+  )
 }
 
 export default Square
