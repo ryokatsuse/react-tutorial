9f4fd919fd9610f8a93244b082c42925a9bb15e4
Ryo Katsuse <ryokatsu0719@gmail.com>
2020-12-30 10:43:44 +0900

    feat:props渡す所まで完成

---
+++ src/views/components/board.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Square from './square'
+
+class Board extends React.Component {
+  renderSquare(i) {
+    return <Square value={i} />;
+  }
+  render() {
+    const status = 'Next player X'
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(3)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+          {this.renderSquare(6)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+          {this.renderSquare(9)}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Board
