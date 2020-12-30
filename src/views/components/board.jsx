90a15f391b3c18e38cdc9e8e5ef9b725db033b3e
Ryo Katsuse <ryokatsu0719@gmail.com>
2020-12-30 11:23:58 +0900

    refactor:propsの状態をboadに集約させhandlerで受け渡すようにした

--- src/views/components/board.jsx
+++ src/views/components/board.jsx
@@ -2,31 +2,51 @@ import React from 'react'
 import Square from './square'
 
 class Board extends React.Component {
-  renderSquare(i) {
-    return <Square value={i} />;
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
   }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
+  }
+
   render() {
-    const status = 'Next player X'
+    const status = 'Next player: X';
+
     return (
       <div>
         <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
-          {this.renderSquare(3)}
+          {this.renderSquare(2)}
         </div>
         <div className="board-row">
+          {this.renderSquare(3)}
           {this.renderSquare(4)}
           {this.renderSquare(5)}
-          {this.renderSquare(6)}
         </div>
         <div className="board-row">
+          {this.renderSquare(6)}
           {this.renderSquare(7)}
           {this.renderSquare(8)}
-          {this.renderSquare(9)}
         </div>
       </div>
-    )
+    );
   }
 }
 
--- src/views/components/square.jsx
+++ src/views/components/square.jsx
@@ -1,19 +1,13 @@
 import React from 'react'
 
 class Square extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      value: null
-    }
-  }
   render() {
     const handler = () => {
-      this.setState({value: 'X'})
+      this.props.onClick()
     }
     return (
       <button className="square" onClick={handler}>
-        {this.state.value}
+        {this.props.value}
       </button>
     )
   }
