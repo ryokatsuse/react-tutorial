81916abf0ecfe013ef6b6e24304c45f2bf577d1f
Ryo Katsuse <ryokatsu0719@gmail.com>
2021-01-01 00:57:38 +0900

    refactor:型推論させるようにした

--- src/views/components/board.tsx
+++ src/views/components/board.tsx
@@ -7,7 +7,7 @@ interface BoardProps {
   onClick: (i: number) => void
 }
 
-const boardNumbers: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+const boardNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 
 const Board: React.FC<BoardProps> = ({squares, onClick}) => {
   return (
